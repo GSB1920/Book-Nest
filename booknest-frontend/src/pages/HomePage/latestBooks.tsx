ff1059465da3a8e0515a17b8627133be6c864934
GSB1920 <gauravbais2001@gmail.com>
2024-08-16 07:12:38 +0530

    Fixed the positions issue of books

--- booknest-frontend/src/pages/HomePage/latestBooks.tsx
+++ booknest-frontend/src/pages/HomePage/latestBooks.tsx
@@ -32,35 +32,7 @@ const LatestBooks = () => {
           coverPage:
             "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBcO26kdi60I4raBpJSMFyJMX2AzPfb2D9Aw&s",
           ratings: null,
-        },
-        {
-          bookName: "BookName",
-          supportText: "Support Text",
-          coverPage:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBcO26kdi60I4raBpJSMFyJMX2AzPfb2D9Aw&s",
-          ratings: null,
-        },
-        {
-          bookName: "BookName",
-          supportText: "Support Text",
-          coverPage:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBcO26kdi60I4raBpJSMFyJMX2AzPfb2D9Aw&s",
-          ratings: null,
-        },
-        {
-          bookName: "BookName",
-          supportText: "Support Text",
-          coverPage:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBcO26kdi60I4raBpJSMFyJMX2AzPfb2D9Aw&s",
-          ratings: null,
-        },
-        {
-          bookName: "BookName",
-          supportText: "Support Text",
-          coverPage:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBcO26kdi60I4raBpJSMFyJMX2AzPfb2D9Aw&s",
-          ratings: null,
-        },
+        }
       ],
     },
     {
@@ -86,20 +58,20 @@ const LatestBooks = () => {
           ratings: null,
         },
       ],
-    }
+    },
   ];
 
   return (
     <div className="flex flex-col justify-center px-60 space-y-10 py-20">
       {Genres.map((genre) => (
-        <div key={genre.sectionName}>
+        <div key={genre.sectionName} className="overflow-hidden">
           <div className="flex justify-between">
-            <div className="text-white text-bold text-3xl">
+            <div className="text-white font-bold text-3xl">
               {genre.sectionName}
             </div>
             <div className="text-yellow-600">See More</div>
           </div>
-          <div className="flex flex-row space-x-16 text-white space-y-4">
+          <div className="flex flex-row space-x-16 text-white py-6">
             {genre.books.map((book) => (
               <div key={book.bookName}>
                 <img
@@ -107,18 +79,18 @@ const LatestBooks = () => {
                   src={book.coverPage}
                   alt="New Arrivals"
                 />
-                <div className="w-64 text-center text-white text-3xl text-bold">
+                <div className="w-64 text-center text-white text-3xl font-bold">
                   {book.bookName}
-                  <div className="w-64 text-center text-white text-xl text-bold">
-                    {book.supportText}
-                  </div>
-                  <div className="w-64 mt-2 text-center">
-                    <img
-                      src="https://w7.pngwing.com/pngs/589/360/png-transparent-star-customer-service-review-rating-star-text-logo-computer-wallpaper.png"
-                      alt=""
-                      className="h-6"
-                    />
-                  </div>
+                </div>
+                <div className="w-64 text-center text-white text-xl font-bold">
+                  {book.supportText}
+                </div>
+                <div className="w-64 mt-2 text-center">
+                  <img
+                    src="https://w7.pngwing.com/pngs/589/360/png-transparent-star-customer-service-review-rating-star-text-logo-computer-wallpaper.png"
+                    alt=""
+                    className="h-6 mx-auto"
+                  />
                 </div>
               </div>
             ))}
